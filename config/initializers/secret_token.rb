e0206767eb25e5fdfa134c6349831bde3a806f60
GtK <GilliTheKid@AirGtK.local>
2011-10-22 19:12:43 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tradenote::Application.config.secret_token = '05f69bebae7be2cd3cf5aeeb81c0424b5cc67d43fd7d450d9013688f9930915973175b6fc5120d335f35d503c54fc1336e1418fb6431f2470143ebe15dc9d427'
