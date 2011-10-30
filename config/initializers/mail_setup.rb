3da9ba9b4794f4e57866d330851bc061b806de24
GtK <GilliTheKid@AirGtK.local>
2011-10-30 13:07:23 +0200

    Mail setup 3

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.clickonward.com",
   :port                 => 25,
-  :domain               => "tradenote.co",
-  :user_name            => "hello@tradenote.co",
+  :domain               => "clickonward.com",
+  :user_name            => "hello@clickonward.com",
   :password             => "0011708",
   :authentication       => "login",
   # changed this from true because of a seg fault (pg only?) - http://redmine.ruby-lang.org/issues/4413
