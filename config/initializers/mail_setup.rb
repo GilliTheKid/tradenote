ec30cf8afb2d74fe679d5765a3ca66138ea6d94f
GtK <GilliTheKid@AirGtK.local>
2011-10-30 13:05:10 +0200

    Mail setup 2

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.upstream.info",
+  :address              => "smtp.clickonward.com",
   :port                 => 25,
   :domain               => "tradenote.co",
   :user_name            => "hello@tradenote.co",
-  :password             => "snelock232",
+  :password             => "0011708",
   :authentication       => "login",
   # changed this from true because of a seg fault (pg only?) - http://redmine.ruby-lang.org/issues/4413
   # set to true for Heroku deploy? -- not sure why?
